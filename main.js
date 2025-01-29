28b46e43461d5b3830afc93a4d2b272ea32ccdea
jj <joshuajonellegarbin.espocia@gmail.com>
2025-01-29 21:54:03 +0800

    change: created a class for file r-w functionality

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
-import readline from 'node:readline'
 import { argv } from 'node:process'
 import fs from 'node:fs/promises'
-
-const r1 = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
-r1.close();
-
 
 class Todo {
 	constructor(args, todos) {
@@ -29,16 +21,16 @@ class Todo {
 			case 1:
 				this.displayTodo();
 				break;
-			case 2:
+			case 3:
 				const argument = this.args[1]
 				if (argument !== 'done' && argument !== 'ongoing') return
 				this.updateTodo(this.args[2], this.args[1] === 'done')
-				writeTodo(this.todos)
+				file.writeTodo(this.todos)
 				break;
 			case 4:
 				if (this.args[1] === 'add') {
-					this.todos.push({ todo: this.args[2], done: this.args[3] === 'true' })
-					writeTodo(this.todos)
+					this.todos.push({ todo: this.args[2], done: this.args[3] === 'true', status: 'show' })
+					file.writeTodo(this.todos)
 					this.displayTodo()
 				}
 				break;
@@ -50,38 +42,47 @@ class Todo {
 	}
 
 }
-async function readTodo() {
-	try {
-		const data = await fs.readFile("data.csv", "utf-8")
-		const entries = data.split('\n').filter((enty) => enty.trim() !== '').map((entry) => {
-			const [todo, done] = entry.split(',')
-			const obj = { todo: todo, done: done === 'true' ? true : false }
-			return obj
-		})
-		return entries.slice(1)
-
-	} catch (err) {
-		console.log(err)
-		return null
+class File {
+	constructor(file) {
+		this.file = file
 	}
-}
 
-async function writeTodo(todo) {
-	try {
-		let arrayToString = "todo,done\n";
-		const parsedString = todo.map((entry) => {
-			return `${entry.todo},${entry.done}\n`;
-		});
-		parsedString.forEach((entry) => { arrayToString += entry });;
+	async readTodo() {
+		try {
+			const data = await fs.readFile(this.file, "utf-8")
+			const entries = data.split('\n').filter((enty) => enty.trim() !== '').map((entry) => {
+				const [todo, done, status] = entry.split(',')
+				const obj = { todo: todo, done: done === 'true' ? true : false, status: status }
+				return obj
+			})
+			return entries.slice(1)
 
-		await fs.writeFile('data.csv', arrayToString)
+		} catch (err) {
+			console.log(err)
+			return null
+		}
+	}
 
-	} catch (err) {
-		cosole.log(err)
+	async writeTodo(todo) {
+		try {
+			let arrayToString = "todo,done,status\n";
+			const parsedString = todo.map((entry) => {
+				return `${entry.todo},${entry.done},${entry.status}\n`;
+			});
+			parsedString.forEach((entry) => { arrayToString += entry });;
+
+			await fs.writeFile('data.csv', arrayToString)
+
+		} catch (err) {
+			cosole.log(err)
+		}
 	}
 }
 
 const argument = argv.slice(2);
-const todos = await readTodo()
-const todo = new Todo(argument, todos)
+const file = new File('data.csv')
+// Reads current todos before initializing
+const infile_todos = await file.readTodo()
+// Initialize todo
+const todo = new Todo(argument, infile_todos)
 todo.init()
