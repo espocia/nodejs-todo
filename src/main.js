5b7bd24782638beeb2809b54008eaf26e00a90f4
jj <joshuajonellegarbin.espocia@gmail.com>
2025-02-20 20:30:39 +0800

    console test

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import { argv } from 'node:process'
 import Todo from './lib/todo.js'
 import File from './lib/file.js'
+console.log('test')
 
 const argument = argv.slice(2);
 const filePath = `${import.meta.dirname}/data/data.csv`
